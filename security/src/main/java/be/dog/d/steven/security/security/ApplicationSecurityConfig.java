09c6e911475305f48d73cd713730a31392f2051a
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-10-29 00:06:34 +0100

    Users & passwords

--- security/src/main/java/be/dog/d/steven/security/security/ApplicationSecurityConfig.java
+++ security/src/main/java/be/dog/d/steven/security/security/ApplicationSecurityConfig.java
@@ -1,23 +1,57 @@
 package be.dog.d.steven.security.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        // Basic authentication:
+        // Basic authentication (always authenticate, no Spring mapping) with whitelisting:
         http
                 .authorizeRequests()
+                .antMatchers("/", "/index", "/css/*", "/js/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        // Creating users - should fetch these from a remote db IRL:
+        // PasswordEncoder needed! (see: PasswordConfig)
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+        UserDetails student = User.builder()
+                .username("student")
+                .password("student")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                admin,
+                student
+        );
     }
 }
