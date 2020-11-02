d36cdfd483c8a65f7a652a9299898c84758c7e74
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-02 06:26:21 +0100

    Initial commit: Starter-mail

--- starter-mail/src/main/java/be/dog/d/steven/startermail/StarterMail.java
+++ starter-mail/src/main/java/be/dog/d/steven/startermail/StarterMail.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarterMail {
 
     public static void main(String[] args) {
-        SpringApplication.run(StarterMail.class, args);
 
+        SpringApplication.run(StarterMail.class, args);
 
     }
 
